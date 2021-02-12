82a9afdb10a5c16e21eb96e67a83b0ac822607ee
DNatanaelFreitas <75276997+DNatanaelFreitas@users.noreply.github.com>
2021-02-12 04:12:40 -0300

    Projeto_01
    Fiz sozinho, tiveram alguns problemas durante o curso mas vou tentar correr atras, estou estudando o basico de novo pois me perdi todo

---
+++ Olá.mundo/src/olá/mundo/OláMundo.java
@@ -0,0 +1,10 @@
+package olá.mundo;
+
+public class OláMundo {
+    //definido como classe publica, todo programa pode acessar
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("olá mundo!!!");
+    }   
+    
+}
